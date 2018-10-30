3eee50b975d8666713b0e34a9f5033cc2c21f977
Xiurong Yu <xiurongy3506@hstat.org>
2018-10-30 04:22:38 +0000

    project website

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+  // Porfolio isotope and filter
+  var portfolioIsotope = $('.portfolio-container').isotope({
+    itemSelector: '.portfolio-item',
+    layoutMode: 'fitRows'
+  });
+
+  $('#portfolio-flters li').on( 'click', function() {
+    $("#portfolio-flters li").removeClass('filter-active');
+    $(this).addClass('filter-active');
+
+    portfolioIsotope.isotope({ filter: $(this).data('filter') });
+  });
+
+ 
